f3180586a6748a65517ed0df24bd634020e39deb
Vitaly <chelyabinsk074@gmail.com>
2026-06-16 19:06:51 +0200

    Set shop phone/WhatsApp to +79227004894

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bloom PWA service worker
-const CACHE = 'bloom-v8';
+const CACHE = 'bloom-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
